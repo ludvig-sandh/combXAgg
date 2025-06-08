b15b59a49dafdf03d8cca73b1bb56003da41b2d7
NikosMet <nikosmetaxakis2003@gmail.com>
2025-06-08 19:30:10 +0300

    Made CC Sync work with the benchmark

--- ds/stacks_cc/stack_impl.h
+++ ds/stacks_cc/stack_impl.h
@@ -13,6 +13,7 @@
 
 #include <ccstack.h>
 #include <primitives.h>
+
 #include "record_manager.h"
 
 template <typename K, typename V>
@@ -24,27 +25,22 @@ class node_t {
 };
 #define nodeptr node_t<K, V> *
 
-class ThreadData
-{
-private:
+class ThreadData {
+   private:
     PAD;
-public:
 
+   public:
     CCStackThreadState *th_state;
 
-    ThreadData()
-    {
-    }
-private:
+    ThreadData() {}
+
+   private:
     PAD;
 };
 
 // PAD;
 ThreadData threadData[MAX_THREADS_POW2];
 PAD;
-
-
-
 
 template <typename K, typename V, class RecManager>
 class Stack {
@@ -59,10 +55,14 @@ class Stack {
     Stack(const int num_threads, const int _min_key, const int _max_key,
           const V _NO_VALUE, unsigned int id)
         : _top(NULL) {
+        object_struct =
+            synchGetAlignedMemory(S_CACHE_LINE_SIZE, sizeof(CCStackStruct));
+        CCStackInit(object_struct, num_threads);
         // CCStackThreadState *th_state;
         // long i, rnum;
         // th_state = reinterpret_cast<CCStackThreadState *>(
-        //     synchGetAlignedMemory(CACHE_LINE_SIZE, sizeof(CCStackThreadState)));
+        //     synchGetAlignedMemory(CACHE_LINE_SIZE,
+        //     sizeof(CCStackThreadState)));
         // CCStackThreadStateInit(object_struct, th_state, (int)id);
     }
     ~Stack() {}
@@ -70,14 +70,17 @@ class Stack {
     V peek(const int &tid) { return NULL; }
 
     bool push(const int &tid, const V &value) {
-        bool success = false;
-        COUTATOMICTID("dummy pushing " << value << std::endl);
+        CCStackPush(object_struct, threadData[tid].th_state, tid, tid);
+        bool success = true;
+        // COUTATOMICTID("dummy pushing " << value << std::endl);
         return success;
     }
 
     bool pop(const int &tid) {
-        bool success = false;
-        COUTATOMICTID("DUMMY popping " << std::endl);
+        CCStackPop(object_struct, threadData[tid].th_state, tid);
+
+        bool success = true;
+        // COUTATOMICTID("DUMMY popping " << std::endl);
         return success;
     }
 
@@ -85,9 +88,11 @@ class Stack {
         // if (init[tid]) return;
         // else init[tid] = !init[tid];
         // recmgr->initThread(tid);
-        threadData[tid].th_state = reinterpret_cast<CCStackThreadState *>( synchGetAlignedMemory(CACHE_LINE_SIZE, sizeof(CCStackThreadState)));
-        
-        CCStackThreadStateInit(object_struct, threadData[tid].th_state, (int)tid);
+        threadData[tid].th_state = reinterpret_cast<CCStackThreadState *>(
+            synchGetAlignedMemory(CACHE_LINE_SIZE, sizeof(CCStackThreadState)));
+
+        CCStackThreadStateInit(object_struct, threadData[tid].th_state,
+                               (int)tid);
     }
 
     void deinitThread(const int tid) {
